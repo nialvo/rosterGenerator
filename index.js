401607b607f812aef3dc5ebce2f41d1438a7ba28
Ignaz Protagonist <pilibili@protonmail.com>
2022-03-04 07:17:54 -0600

    tested and fixed classes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const Engineer = require('./lib/Engineer.js')
+const Manager = require('./lib/Manager.js')
+const Intern = require('./lib/Intern.js')
+
+const alec =new Engineer("a","b","c","dd");
+
+console.log(alec.getRole());
